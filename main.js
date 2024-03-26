5a919d309e1e6b31a70175186d30f7dc9415df10
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-27 03:46:40 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+let myBalance = 18000; //dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+console.log(pinAnswer.pin);
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code .");
+    let operationAnswer = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ['withdraw', 'check balance']
+        }
+    ]);
+    console.log(operationAnswer.operation);
+    if (operationAnswer.operation === 'withdraw') {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "list",
+                choices: ['500', '1000', '3000', '5000', '7000', '10000', '15000', '20000',]
+            }
+        ]);
+        console.log(amountAns.amount);
+        if (amountAns.amount > myBalance) {
+            console.log('Your balance is insufficient for this transaction. ');
+        }
+        else
+            myBalance -= amountAns.amount;
+        console.log(`Your balance is: ${myBalance}`);
+    }
+    else {
+        console.log(myBalance);
+    }
+}
+else {
+    console.log("incorrect pin code");
+}
